b89141d7b7cacbf383848e69ce6a3b62c61c2a61
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-10-05 22:19:35 -0600

    Pet details Modal added

--- js/index.js
+++ js/index.js
@@ -35,18 +35,25 @@ const displayAllPets = (pets) => {
 
   pets.forEach((pet) => {
     const card = document.createElement("div");
-    card.innerHTML = 
-    ` 
+    card.innerHTML = ` 
     <div class="flex flex-col justify-center space-y-2 p-2 border-2 rounded-lg">
             <div>
-              <img class="rounded-xl" src="${pet.image}" alt="Image of pet" />
+              <img class="rounded-xl h-full w-full object-cover" src="${
+                pet.image
+              }" alt="Image of pet" />
             </div>
             <div class="">
               <h1 class="text-xl font-bold">${pet.pet_name}</h1>
               <p>Breed: ${pet.breed ? pet.breed : "Not Available"}</p>
-              <p><i class="fa-regular fa-calendar"></i> Birth: ${pet.date_of_birth ? pet.date_of_birth : "Not Available"}</p>
-              <p><i class="fa-solid fa-mercury"></i> Gender: ${pet.gender ? pet.gender : "Not Available"}</p>
-              <p><i class="fa-solid fa-dollar-sign"></i> Price: ${pet.price ? pet.price : "Not Available"}</p>
+              <p><i class="fa-regular fa-calendar"></i> Birth: ${
+                pet.date_of_birth ? pet.date_of_birth : "Not Available"
+              }</p>
+              <p><i class="fa-solid fa-mercury"></i> Gender: ${
+                pet.gender ? pet.gender : "Not Available"
+              }</p>
+              <p><i class="fa-solid fa-dollar-sign"></i> Price: ${
+                pet.price ? pet.price : "Not Available"
+              }</p>
               <hr />
             </div>
             <div class="flex gap-2 justify-center">
@@ -54,7 +61,9 @@ const displayAllPets = (pets) => {
                 <i class="fa-regular fa-thumbs-up"></i>
               </button>
               <button class="btn bg-white border-1 text-teal-800">Adopt</button>
-              <button class="btn bg-white border-1 text-teal-800">
+              <button onclick="petDetails(${
+                pet.petId
+              })" class="btn bg-white border-1 text-teal-800">
                 Details
               </button>
             </div>
@@ -62,9 +71,52 @@ const displayAllPets = (pets) => {
 
     `;
 
-    //add button to catagory container
     cardsConainter.append(card);
   });
+};
+
+petDetails = async (id) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/peddy/pet/${id}`
+  );
+
+  const data = await response.json();
+  const pet = data.petData
+  console.log(pet.image)
+
+  const modalBox = document.getElementById("modal-content");
+
+  modalBox.innerHTML = `
+   <div>
+        <img class="rounded-xl h-full w-full object-cover" src="${
+        pet.image
+        }" alt="Image of pet" />
+    </div>
+    <div class="space-y-1">
+        <h1 class="text-xl font-bold">${pet.pet_name}</h1>
+        <p>Breed: ${pet.breed ? pet.breed : "Not Available"}</p>
+        <p><i class="fa-regular fa-calendar"></i> Birth: ${
+        pet.date_of_birth ? pet.date_of_birth : "Not Available"
+        }</p>
+        <p><i class="fa-solid fa-mercury"></i> Gender: ${
+        pet.gender ? pet.gender : "Not Available"
+        }</p>
+        <p><i class="fa-solid fa-dollar-sign"></i> Price: ${
+        pet.price ? pet.price : "Not Available"
+        }</p>
+        <hr />
+    </div>
+    <div class="space-y-4">
+          <h2 class="font-bold">Details Information</h2>
+          <p>${pet.pet_details}</p>
+          <form method="dialog">
+            <button class="btn w-full bg-teal-300">Cancel</button>
+          </form>
+        
+    </div>
+  `;
+
+  document.getElementById("petModal").showModal();
 };
 
 loadCategories();
